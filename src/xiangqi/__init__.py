1687f268e136ccb9e8081e34134945577793ad4c
Augustus Seabrooke <august.seabrooke@gmail.com>
2026-09-23 14:37:35 -0700

    Add src/xiangqi/__init__.py

---
+++ src/xiangqi/__init__.py
@@ -0,0 +1,5 @@
+from .board import Board
+from .game import Game, GameState
+from .models import Color, Piece, PieceType, Position
+
+__all__ = ["Board", "Color", "Game", "GameState", "Piece", "PieceType", "Position"]
