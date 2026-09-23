50455533f114ba70237b8e3988604b16783f8c01
Augustus Seabrooke <august.seabrooke@gmail.com>
2026-09-23 14:43:25 -0700

    Export move history model

--- src/xiangqi/__init__.py
+++ src/xiangqi/__init__.py
@@ -1,5 +1,14 @@
 from .board import Board
 from .game import Game, GameState
-from .models import Color, Piece, PieceType, Position
+from .models import Color, MoveRecord, Piece, PieceType, Position
 
-__all__ = ["Board", "Color", "Game", "GameState", "Piece", "PieceType", "Position"]
+__all__ = [
+    "Board",
+    "Color",
+    "Game",
+    "GameState",
+    "MoveRecord",
+    "Piece",
+    "PieceType",
+    "Position",
+]
